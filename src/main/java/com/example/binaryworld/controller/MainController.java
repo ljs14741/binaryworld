b526a5d7aed049b75071361763860e4969c6b53f
binary <ljs14741@naver.com>
2025-07-02 20:38:31 +0900

    About / Privacy Policy / Contact 페이지 생성

--- src/main/java/com/example/binaryworld/controller/MainController.java
+++ src/main/java/com/example/binaryworld/controller/MainController.java
@@ -9,6 +9,20 @@ public class MainController {
     @GetMapping("/")
     public String main() {
         return "main";
-        //
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "common/about";
+    }
+
+    @GetMapping("/privacy-policy")
+    public String privacyPolicy() {
+        return "common/privacy-policy";
+    }
+
+    @GetMapping("/contact")
+    public String contact() {
+        return "common/contact";
     }
 }
